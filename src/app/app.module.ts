59c9b61255abfde0662825658519a017a7f68d99
luisangel584 <luis_angel584@hotmail.com>
2020-02-12 02:14:06 -0600

    set origin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFirestoreModule, AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot(),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
   ],
-  providers: [],
+  providers: [AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCEr0npyG1i8Bu1e1MkcK20KRVZ2fEHf4g',
+    authDomain: 'ucash-9d489.firebaseapp.com',
+    databaseURL: 'https://ucash-9d489.firebaseio.com',
+    projectId: 'ucash-9d489',
+    storageBucket: 'ucash-9d489.appspot.com',
+    messagingSenderId: '243779534865',
+    appId: '1:243779534865:web:3e651e2142f3f424e3f1b0',
+    measurementId: 'G-JJQLEYH1B2'
+  }
 };
