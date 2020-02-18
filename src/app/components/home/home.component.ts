8107660f2dda1d0264f5dac9853f2dccc9903d5d
SoniaHer <sonia.h@yocontigo-it.com>
2020-02-18 14:05:57 -0600

    Primera parte del Home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public chartType: string = 'bar';
+
+  public chartDatasets: Array<any> = [
+    { data: [65, 59, 80, 81,], label: 'Simulador de Inversión' }
+  ];
+
+  public chartLabels: Array<any> = ['6', '12', '18', '24'];
+
+  public chartColors: Array<any> = [
+    {
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 2,
+    }
+  ];
+
+  public chartOptions: any = {
+    responsive: true
+  };
+  public chartClicked(e: any): void { }
+  public chartHovered(e: any): void { }
+
+
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
