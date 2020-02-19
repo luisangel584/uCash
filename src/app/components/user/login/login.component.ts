d7229d71c09f6e40f15cf918bf51d02354bc59c2
luisangel584 <luis_angel584@hotmail.com>
2020-02-19 09:50:10 -0600

    Modify redirect routes after login or register

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -30,7 +30,7 @@ export class LoginComponent implements OnInit {
 
     this.auth.loginUser(this.user)
       .subscribe((res) => {
-        this.router.navigateByUrl('/usuario/perfil');
+        this.router.navigateByUrl('/panel');
       },
       (err) => {
         console.log(err.error.error.message);
