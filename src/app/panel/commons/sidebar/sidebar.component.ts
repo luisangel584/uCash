db326afda5373ec94c6013c8f0f32a1e1d36811b
luisangel584 <luis_angel584@hotmail.com>
2020-02-20 12:39:30 -0600

    create service for read and update data

--- src/app/panel/commons/sidebar/sidebar.component.ts
+++ src/app/panel/commons/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public auth: AuthService
+  ) { }
 
   ngOnInit() {
+  }
+
+  logout() {
+    this.auth.logout();
   }
 
 }
--- src/app/panel/data/situacion-laboral/situacion-laboral.component.ts
+++ src/app/panel/data/situacion-laboral/situacion-laboral.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-situacion-laboral',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SituacionLaboralComponent implements OnInit {
 
-  constructor() { }
+  info = {};
+  loading = false;
+
+  constructor(
+    public dataService: DataService
+  ) { }
 
   ngOnInit() {
+    this.loading = true;
+    this.dataService.getDataCollection('situacion-laboral')
+      .then((resp) => {
+        this.info = resp;
+        setTimeout(() => {
+          this.loading = false;
+        }, 1000);
+      });
+  }
+
+  onSubmit() {
+    this.loading = true;
+    this.dataService.setDataCollection('situacion-laboral', this.info)
+      .then(() => {
+        setTimeout(() => {
+          this.loading = false;
+        }, 1000);
+      });
   }
 
 }
