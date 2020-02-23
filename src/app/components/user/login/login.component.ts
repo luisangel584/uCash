fc2ffb81e4083cc78cc826d284a4bcf562651878
luisangel584 <luis_angel584@hotmail.com>
2020-02-22 23:29:40 -0600

    set server-response validation in forms for login and register && animation in forms component

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -13,6 +13,8 @@ import { AuthService } from 'src/app/services/auth.service';
 export class LoginComponent implements OnInit {
 
   user: UserModel;
+  isError: string;
+  recordarUsuario = false;
 
   constructor(
     private auth: AuthService,
@@ -21,20 +23,82 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
     this.user = new UserModel();
+    if (localStorage.getItem('uCashLoginMail')) {
+      this.user.email = localStorage.getItem('uCashLoginMail');
+      this.recordarUsuario = true;
+    }
   }
 
   onSubmit(form: NgForm) {
-    console.log('Formulario enviado');
-    console.log(this.user);
-    console.log(form);
+    // console.log('Formulario enviado');
+    // console.log(this.user);
+    // console.log(form);
 
     this.auth.loginUser(this.user)
       .subscribe((res) => {
         this.router.navigateByUrl('/panel');
+
+        if (this.recordarUsuario) { 
+          localStorage.setItem('uCashLoginMail', this.user.email);
+        } else {
+          localStorage.removeItem('uCashLoginMail');
+        }
       },
       (err) => {
-        console.log(err.error.error.message);
+        console.log(err.error.error.code, err.error.error.message);
+        this.sendError(err.error.error.message);
+
+        // this.isError = true;
       });
+  }
+
+  sendError(err: string) {
+    //
+    let msg: string;
+
+    switch (err) {
+      case 'MISSING_EMAIL': {
+        msg = 'Ingrese correo electrónico';
+        break;
+      }
+      case 'MISSING_PASSWORD': {
+        msg = 'Ingrese contraseña';
+        break;
+      }
+      case 'INVALID_PASSWORD': {
+        msg = 'Contraseña no válida';
+        break;
+      }
+      case 'INVALID_EMAIL': {
+        msg = 'Correo electrónico no válido';
+        break;
+      }
+      case 'EMAIL_NOT_FOUND': {
+        msg = 'No se encontró una cuenta asociada al email especificado';
+        break;
+      }
+      default: {
+        msg = 'Verifique los datos';
+      }
+    }
+
+    this.isError = msg;
+
+    setTimeout(() => {
+      this.isError = undefined;
+    }, 5000);
+  }
+
+  closeError() {
+    this.isError = undefined;
+  }
+
+  checkRememberUser() {
+    if (this.recordarUsuario) {
+      console.log('ok');
+    } else {
+      console.log('no');
+    }
   }
 
 }
--- src/app/components/user/register/register.component.ts
+++ src/app/components/user/register/register.component.ts
@@ -13,6 +13,8 @@ import { DataService } from 'src/app/services/data.service';
 export class RegisterComponent implements OnInit {
 
   user: UserModel;
+  isError: string;
+  recordarUsuario = false;
 
   constructor(
     private auth: AuthService,
@@ -22,6 +24,11 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
     this.user = new UserModel();
+
+    if (localStorage.getItem('uCashLoginMail')) {
+      this.user.email = localStorage.getItem('uCashLoginMail');
+      this.recordarUsuario = true;
+    }
   }
 
   onSubmit(f) {
@@ -30,10 +37,48 @@ export class RegisterComponent implements OnInit {
       this.dataService.firstSession(res['localId'], res['email'])
         .then(() => {
           this.router.navigateByUrl('/panel');
+
+          if (this.recordarUsuario) { 
+            localStorage.setItem('uCashLoginMail', this.user.email);
+          }
         });
     }, (err) => {
       console.log(err.error.error.message);
+      this.sendError(err.error.error.message);
     });
+  }
+
+  sendError(err: string) {
+    //
+    let msg: string;
+
+    switch (err) {
+      case 'WEAK_PASSWORD : Password should be at least 6 characters': {
+        msg = 'Constraseña debe contener al menos 6 caracteres';
+        break;
+      }
+      case 'MISSING_PASSWORD': {
+        msg = 'Ingrese una contraseña';
+        break;
+      }
+      case 'EMAIL_EXISTS': {
+        msg = 'Ya existe un usuario con el email especificado';
+        break;
+      }
+      default: {
+        msg = 'Verifique los datos';
+      }
+    }
+
+    this.isError = msg;
+
+    setTimeout(() => {
+      this.isError = undefined;
+    }, 5000);
+  }
+
+  closeError() {
+    this.isError = undefined;
   }
 
 }
